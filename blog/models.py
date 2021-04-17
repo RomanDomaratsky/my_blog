cf5483a3092c3cb8989ae86a480511ff3d2e2be6
Pose1don666 <roma.domaratsky@gmail.com>
2021-04-17 20:26:10 +0300

    added blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=300)
+    blog_date = models.DateTimeField()
+    blog_text = models.CharField(max_length=300)
+    blog_image = models.ImageField(upload_to='blog_images/')
